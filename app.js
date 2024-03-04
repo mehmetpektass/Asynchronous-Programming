a45756e5637232f6e18124d94b2be9951ba9fc51
mehmetpektass <pektasmehmett1184@gmail.com>
2024-03-04 13:59:43 +0300

    edited app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("mehmet");
+
+setTimeout(() => {
+    console.log("1 seconds past")
+}, 1000);
+
+setTimeout(() => {
+    console.log("999 ms past")
+}, 999);
+
+console.log("pektas")
+
+
+/* 
+
+Asynchronous Programming
+
+ 1- Timing
+ 2- Events
+ 3- HTTP Request
+ */
